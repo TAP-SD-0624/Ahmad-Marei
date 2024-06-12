fe0516a352a185b07d576b82661f2b319801a748
Ahmad Marei <82359651+101ahmadmarei@users.noreply.github.com>
2024-06-12 12:42:18 +0300

    feat: fix styles

--- script.js
+++ script.js
@@ -6,26 +6,32 @@ document.addEventListener("DOMContentLoaded", function () {
   const cardContainer = document.getElementById("card-container");
   const sortBySelect = document.getElementById("sort-by");
   const filterBySelect = document.getElementById("filter-by");
-
+  const favoriteContainer = document.getElementById("favorite_items");
+  const toggleFavorites = document.getElementById("toggle-favorites");
+  const favoriteItems = document.getElementById("favorite_items-container");
   toggleButton.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
+  });
+  toggleFavorites.addEventListener("click", function () {
+    favoriteItems.classList.toggle("hidden");
   });
   searchInput.addEventListener("input", function () {
     updateCards(searchInput.value.trim().toLowerCase());
   });
+
   sortBySelect.addEventListener("change", function () {
-    updateDisplay();
+    // updateDisplay();
   });
 
   filterBySelect.addEventListener("change", function () {
-    updateDisplay();
+    // updateDisplay();
   });
   const numberOfCards = document.getElementById("number-of-cards");
 
   numberOfCards.innerHTML = `"${data.length}" Web Topics Found`;
 
   function updateCards(query) {
-    cardContainer.innerHTML = ""; // Clear the container before adding new filtered cards
+    cardContainer.innerHTML = "";
     const filteredData = data.filter(
       (item) =>
         item.topic.toLowerCase().includes(query) ||
@@ -42,6 +48,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const numberOfCards = document.getElementById("number-of-cards");
     numberOfCards.innerHTML = `"${filteredData.length}" Web Topics Found`;
   }
+
+  function updateFavoriteCards() {
+    favoriteContainer.innerHTML = ""; // Clear the container before adding new filtered cards
+    const favoriteData = data.slice(0, 3);
+
+    favoriteData.forEach((item) => {
+      const card = document.createElement("div");
+      card.className = "card card-favorite";
+      card.innerHTML = generateFavoriteCard(item);
+      favoriteContainer.appendChild(card);
+    });
+  }
+
   function generateStars(rating) {
     const fullStars = Math.floor(rating);
     const halfStars = rating % 1 ? 1 : 0;
@@ -53,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function generateCard(item) {
     return `
-    <img src="${item.image}" alt="${item.topic}" class="card-image">
+    <img src="${item.image}" alt="${item.topic}" class="card-image ">
     <div class="card-content">
       <h3 class="card-subtitle truncate">${item.category}</h3>
       <p class="card-title truncate">${item.topic}</p>
@@ -64,5 +83,18 @@ document.addEventListener("DOMContentLoaded", function () {
     </div>
     `;
   }
+
+  function generateFavoriteCard(item) {
+    return `
+    <img src="${item.image}" alt="${item.topic}" class="card-image">
+    <div class="card-content">
+      <p class="card-title truncate">${item.topic}</p>
+      <div class="card-rating">
+        ${generateStars(item.rating)}
+      </div>
+    </div>
+    `;
+  }
   updateCards("");
+  updateFavoriteCards("");
 });
